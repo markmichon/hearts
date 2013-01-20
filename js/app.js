230ced52a26146fc6e42344e893a8311f021854e
Mark Michon <michon@gmail.com>
2013-01-20 00:44:17 -0500

    working, unoptimized version

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
   var startWidth = $(window).height() + 'px';
   heart.css('fontSize',startWidth);
   // container.width($(window).height());
-  $(container).on('mouseenter', '.heart', splitHeart);
+  $(container).on('mouseenter', '.heart', splitHeart).delay(5000);
 
 });
 // var heartTemplate = '<div class="hearts"><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div></div>';
@@ -37,16 +37,18 @@ function splitHeart() {
   fontSize = parseInt(fontSize, 10);
   console.log(fontSize);
   newSize = (fontSize / 2) + 'px';
-  newWidth = (fontSize / 2) * .75 + 'px';
+  newWidth = (fontSize / 2) + 'px';
   console.log(newSize);
   heartObject = $(heartTemplate);
   heartObject.find('.heart').each(function(){
     $(this).css('fontSize', newSize);
     $(this).css('color', pickColor());
-    $(this).css('width', newWidth);
+    // $(this).css('width', newWidth);
   });
   // $(this).clone().appendTo($('.hearts'));
-  $(this).replaceWith(heartObject);
+  // $(this).fadeOut('fast');
+  $(this).replaceWith($(heartObject).fadeIn("slow"));
+
   console.log($('.heart').css('fontSize'));
 };
 function makeHeart() {
@@ -56,7 +58,7 @@ function makeHeart() {
 }
 
 function pickColor() {
-  var colors = [ '#facade', '#f9bad4', '#f7abcb', '#f69bc1', '#f48cb7']
+  var colors = [ '#facade', '#f9bad4', '#f7abcb', '#f69bc1', '#f48cb7', '#333']
   var random = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
   return colors[random];
 }
