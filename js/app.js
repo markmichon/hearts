fa69eaffc2e0a9f06d3b097b48b6d31183f9d4a4
Mark Michon <michon@gmail.com>
2013-01-20 00:17:15 -0500

    semi-working. some shifting still occurs

--- js/app.js
+++ js/app.js
@@ -1,34 +1,58 @@
 $(function() {
   var container = $('.hearts');
   var heart = $('.heart');
-  container.width($(window).height());
+  var startWidth = $(window).height() + 'px';
+  heart.css('fontSize',startWidth);
+  // container.width($(window).height());
   $(container).on('mouseenter', '.heart', splitHeart);
 
 });
-var heartTemplate = '<div class="hearts"><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div></div>';
-function splitHeart() {
-  width = $(this).width();
-  height = $(this).height();
-  newWidth = width / 2;
-  newHeight = height / 2;
-  $(this).width(newWidth + 1);
-  $(this).height(newWidth + 1);
-  console.log(heartTemplate);
-  heartObject = $(heartTemplate);
-  heartObject.find('img').each(function(){
-    this.width = newWidth;
-    this.height = newWidth;
-    $(this).css('color', pickColor());
-  });
+// var heartTemplate = '<div class="hearts"><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div><div class="row"><img class="heart" src="img/heart.svg" /><img class="heart" src="img/heart.svg" /></div></div>';
 
-  console.log(heartObject.width(width));
-  console.log(heartObject);
+// function splitHeart() {
+//   width = $(this).width();
+//   height = $(this).height();
+//   newWidth = width / 2;
+//   newHeight = height / 2;
+//   $(this).width(newWidth + 1);
+//   $(this).height(newWidth + 1);
+//   console.log(heartTemplate);
+//   heartObject = $(heartTemplate);
+//   heartObject.find('img').each(function(){
+//     this.width = newWidth;
+//     this.height = newWidth;
+//     $(this).css('color', pickColor());
+//   });
+
+//   console.log(heartObject.width(width));
+//   console.log(heartObject);
+//   // $(this).clone().appendTo($('.hearts'));
+//   $(this).replaceWith(heartObject);
+// };
+
+/* Font Version */
+heartTemplate = makeHeart();
+function splitHeart() {
+  fontSize = $(this).css('fontSize');
+  fontSize = parseInt(fontSize, 10);
+  console.log(fontSize);
+  newSize = (fontSize / 2) + 'px';
+  newWidth = (fontSize / 2) * .75 + 'px';
+  console.log(newSize);
+  heartObject = $(heartTemplate);
+  heartObject.find('.heart').each(function(){
+    $(this).css('fontSize', newSize);
+    $(this).css('color', pickColor());
+    $(this).css('width', newWidth);
+  });
   // $(this).clone().appendTo($('.hearts'));
   $(this).replaceWith(heartObject);
+  console.log($('.heart').css('fontSize'));
 };
-
-function makeHeart(elem) {
-  elem.appendTo($('.hearts'));
+function makeHeart() {
+  var heartNode = '<div class="heart lsf">heart</div>';
+  heartTemplate = '<div class="hearts">' + '<div class="row">' + heartNode + heartNode + '</div><div class="row">' + heartNode + heartNode + '</div></div>';
+  return heartTemplate;
 }
 
 function pickColor() {
